41884ac89fcafbe2e018dce435b3fa0c7508d448
Himanshu Agarwal <himanshuagarwal190@gmail.com>
2020-11-08 20:27:25 +0530

    Add functionality for rooms and online users

--- app.js
+++ app.js
@@ -1,26 +1,94 @@
 const express = require('express');
 const socket = require('socket.io');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+var port = process.env.PORT || 3000;
 
+//Store connected Users
+var users = {}
+
+//Funtion to get users online in a room
+function getUsers(arr){
+    onlineUsers = []
+    arr.forEach((onlineUser) => {
+        onlineUsers.push(Object.values(onlineUser)[0])
+    })
+    return onlineUsers
+}
+
+//Render Index page
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log('Server Running')
+//Get username and roomname from form and pass it to room
+app.post('/room', (req, res) => {
+    roomname = req.body.roomname;
+    username = req.body.username;
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
+
+app.get('/room', (req, res)=>{
+    res.render('room')
+})
+
+//Start Server
+const server = app.listen(port, () => {
+    console.log(`Server Running on ${port}`)
 })
 
 const io = socket(server);
 
+//Socket connection
 io.on('connection', (socket) => {
-    socket.on('chat', (data) =>{
-        io.sockets.emit('chat', data);
+
+    socket.on('joined-user', (data) =>{
+        //Storing users connected in a room in memory
+        var user = {};
+        user[socket.id] = data.username;
+        if(users[data.roomname]){
+            users[data.roomname].push(user);
+        }
+        else{
+            users[data.roomname] = [user];
+        }
+        
+        //Joining the Socket Room
+        socket.join(data.roomname);
+
+        //Emitting New Username to Clients
+        io.to(data.roomname).emit('joined-user', {username: data.username});
+
+        //Send online users array
+        io.to(data.roomname).emit('online-users', getUsers(users[data.roomname]))
     })
 
+    //Emitting messages to Clients
+    socket.on('chat', (data) =>{
+        io.to(data.roomname).emit('chat', {username: data.username, message: data.message});
+    })
+
+    //Broadcasting the user who is typing
     socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', data)
+        socket.broadcast.to(data.roomname).emit('typing', data.username)
+    })
+
+    //Remove user from memory when they disconnect
+    socket.on('disconnecting', ()=>{
+        var rooms = Object.keys(socket.rooms);
+        var socketId = rooms[0];
+        var roomname = rooms[1];
+        users[roomname].forEach((user, index) => {
+            if(user[socketId]){
+                users[roomname].splice(index, 1)
+            }
+        });
+
+        //Send online users array
+        io.to(roomname).emit('online-users', getUsers(users[roomname]))
     })
 })
--- public/chat.js
+++ public/chat.js
@@ -1,29 +1,66 @@
 const output = document.getElementById('output');
-const handle = document.getElementById('handle');
 const message = document.getElementById('message');
 const send = document.getElementById('send');
-const feedback = document.getElementById('feedback')
+const feedback = document.getElementById('feedback');
+const roomMessage = document.querySelector('.room-message');
+const users = document.querySelector('.users');
 
+//Socket server URL
 const socket = io.connect('https://a-chatting-app.herokuapp.com');
 
+//Fetch URL Params from URL
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const username = urlParams.get('username');
+const roomname = urlParams.get('roomname');
+console.log(username, roomname);
 
+//Display the roomname the user is connected to
+roomMessage.innerHTML = `Connected in room ${roomname}`
+
+//Emitting username and roomname of newly joined user to server
+socket.emit('joined-user', {
+    username: username,
+    roomname: roomname
+})
+
+//Sending data when user clicks send
 send.addEventListener('click', () =>{
     socket.emit('chat', {
-        handle: handle.value,
-        message: message.value
+        username: username,
+        message: message.value,
+        roomname: roomname
     })
     message.value = '';
 })
 
+//Sending username if the user is typing
 message.addEventListener('keypress', () => {
-    socket.emit('typing', {handle: handle.value})
+    socket.emit('typing', {username: username, roomname: roomname})
 })
 
+//Displaying if new user has joined the room
+socket.on('joined-user', (data)=>{
+    output.innerHTML += '<p>--> <strong><em>' + data.username + ' </strong>has Joined the Room</em></p>';
+})
+
+//Displaying the message sent from user
 socket.on('chat', (data) => {
-    output.innerHTML += '<p><strong>' + data.handle + '</strong>: ' + data.message + '</p>';
+    output.innerHTML += '<p><strong>' + data.username + '</strong>: ' + data.message + '</p>';
     feedback.innerHTML = '';
+    document.querySelector('.chat-message').scrollTop = document.querySelector('.chat-message').scrollHeight
+
 })
 
-socket.on('typing', (data) => {
-    feedback.innerHTML = '<p><em>' + data.handle + ' is typing...</em></p>';
+//Displaying if a user is typing
+socket.on('typing', (user) => {
+    feedback.innerHTML = '<p><em>' + user + ' is typing...</em></p>';
+})
+
+//Displaying online users
+socket.on('online-users', (data) =>{
+    users.innerHTML = ''
+    data.forEach(user => {
+        users.innerHTML += `<p>${user}</p>`
+    });
 })
