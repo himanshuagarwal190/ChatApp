5bb95ba670d5a101a2420c407114e8e0bb7c63d5
Himanshu Agarwal <himanshuagarwal@MB-M-HIMANSHUA.local>
2024-04-16 09:20:57 +0530

    remove hardcoded url

--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,7 @@ const roomMessage = document.querySelector('.room-message');
 const users = document.querySelector('.users');
 
 //Socket server URL
-const socket = io.connect('https://a-chatting-app.herokuapp.com');
+const socket = io.connect();
 
 //Fetch URL Params from URL
 const queryString = window.location.search;
